85e7abd84d475aaf4929c8bf5778efd521d1741f
vaishali <vaishalibuddha56@gmail.com>
2026-09-19 20:18:17 +0530

    Initial commit

---
+++ first.js
@@ -0,0 +1,52 @@
+
+// let a=5;
+// let b=3;
+
+// {
+
+// // arithmetic operator
+//     // a++;
+//     // b--;
+//     console.log("a + b =", a + b);
+//     console.log("a - b =", a - b);
+//     console.log("a * b =", a*b);
+//     console.log("a / b =", a / b);
+//     console.log(" a ** b =" , a**b);
+//     console.log("a =",a);
+//     console.log("b =",b);
+
+
+// //assignment operator
+
+
+// console.log("a = b ", a = b);
+// console.log("a += a ", a += a);
+// console.log("a -= a ", a -= a);
+// console.log("a *= a ", a *= a);
+// console.log("a %= a ", a %= a);
+// console.log("a **= b ", a **= b);
+
+
+
+
+// // camparison operator
+//  console.log("a != b", a != b);
+//  console.log("a === b", a===b);
+//  console.log("a !== b", a !==b);
+//   console.log("a > b", a > b);
+//  console.log("a >= b", a >= b);
+//  console.log("a < b", a < b);
+//  console.log("a <=  b", a<= b);
+
+//  logical AND
+let a = 6;
+let b = 5;
+
+let cont1 = a < b;
+let cont2 = a === 5;
+console.log("cont1 || = cont2 ", cont1  ||  cont2 );
+
+
+
+
+
